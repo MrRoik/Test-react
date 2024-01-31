681998a21257abedc7cceb4b8a73d2855ed58dad
MrRoik <138239220+MrRoik@users.noreply.github.com>
2024-01-31 16:45:59 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,9 @@
-import { Product } from "./Product";
+//import { useMemo } from 'react';
+import { UserMenu } from './UserMenu';
+import { useUser } from '../userContext.jsx';
 
-export default function App() {
-  return (
-    <div>
-      <h1>Best selling</h1>
+export const App = () => {
+  const { isLoggedIn } = useUser();
 
-      <Product
-        name="Tacos With Lime"
-        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-        price={10.99}
-      />
-      <Product
-        name="Fries and Burger"
-        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
-        price={14.29}
-      />
-    </div>
-  );
-}
+  return <div>{isLoggedIn && <UserMenu />}</div>;
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/App.jsx'
-import './index.css'
+//import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './components/App.jsx';
+import './index.css';
+//import { userContext } from './userContext.js';
+import { UserProvider } from './userContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  //<React.StrictMode>
+  //<App />
+  //</React.StrictMode>
+  //<userContext.Provider value={contextValue}>
+  //  <App />
+  //</userContext.Provider>
+  <UserProvider>
     <App />
-  </React.StrictMode>,
-)
+  </UserProvider>
+);
